e1db0490ba35eaf87b4df5d547d56a5fbc4a486c
xThrasherrr <101474430+xThrasherrr@users.noreply.github.com>
2023-05-19 10:49:16 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-description 'Script Template for RedM QR-Core | xThrasherrr#6666'
+description 'Simple fast travel resource for qr-core'
 author 'xThrasherrr#6666'
 
 shared_scripts { '@ox_lib/init.lua', 'modules/shared.lua', 'shared/*.lua' }
